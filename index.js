c6515e0eca33e84b677f79881c95ec6c6cc0750e
Alex Grover <alexat955@gmail.com>
2016-05-23 10:58:57 -0700

    React prefex for rules

--- index.js
+++ index.js
@@ -7,9 +7,8 @@ module.exports = {
     'max-len': 0,
     'no-underscore-dangle': [2, { 'allowAfterThis': true }],
     'no-unresolved': 0,
-    'prefer-stateless-function': 0,
     'react/prefer-stateless-function': 0,
+    'react/sort-comp': 0,
     'semi': [2, 'never'],
-    'sort-comp': 0,
   },
 };
